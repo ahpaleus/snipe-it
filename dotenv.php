99abba86aaca4e69d1fdee89efdcdb362ad70002
Maciej Domanski <38883201+ahpaleus@users.noreply.github.com>
2022-03-15 10:43:59 +0100

    Update dotenv.php

--- dotenv.php
+++ dotenv.php
@@ -3,7 +3,7 @@ use Dotenv\Dotenv;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv::create(dirname(__DIR__), 'snipe-it/.env.phpstan');
+$dotenv = Dotenv::create(dirname(__DIR__), 'snipe-it/.env.testing');
 $dotenv->load(__DIR__.'/.env');
 
 ?>
